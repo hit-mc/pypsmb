cce2b651840968a15141de51bc8224c41b5011d5
Keuin <keuinx@gmail.com>
2022-03-17 19:08:47 +0800

    Improve log messages.

--- pypsmb/mb/client_handler.py
+++ pypsmb/mb/client_handler.py
@@ -25,55 +25,54 @@ def validate_pattern(s: str):
 
 
 def _publish(sock: socket.socket, addr, dispatcher: MessageDispatcher,
-             topic_id: str, keep_alive: float, logger, max_pending_keepalive: int = 3,
+             topic_id: str, keep_alive: float, max_pending_keepalive: int = 3,
              protocol: int = 1):
+    logger = logging.getLogger('publish,%s:%d' % addr)
     pending_keepalive_count = 0  # how many continuous NOP did we sent, which is not responded by the client
-    try:
-        while True:
-            logger.info('Waiting for client command...')
-            rlist, _, _ = select.select([sock], [], [], keep_alive if (keep_alive > 0) else None)
-            if not rlist:
-                if protocol != 2:
-                    # protocol v1 does not support NOP sent from passive peer
-                    continue
-                # timeout, send keepalive
-                if pending_keepalive_count == max_pending_keepalive:
-                    # the client is not sensible
-                    # kick it
-                    logger.error('Insensible client (too many pending keepalive responses). Kick it.')
-                    break
-                logger.info('Send NOP. (keepalive)')
-                sock.sendall(b'NOP')
-                logger.info('NOP is sent.')
-                pending_keepalive_count += 1
+    while True:
+        logger.info('Waiting client for commands...')
+        rlist, _, _ = select.select([sock], [], [], keep_alive if (keep_alive > 0) else None)
+        if not rlist:
+            if protocol != 2:
+                # protocol v1 does not support NOP sent from passive peer
                 continue
-            command = read_exactly(sock, 3)
-            if command == b'NOP':
-                logger.info('Client NOP.')
-                sock.sendall(b'NIL')
-                logger.info('Responded with NIL.')
-            elif command == b'NIL':
-                logger.info('Client is OK.')
-                pending_keepalive_count = 0
-                pass
-            elif command == b'BYE':
-                logger.info('Client BYE. Disconnecting.')
+            # timeout, send keepalive
+            if pending_keepalive_count == max_pending_keepalive:
+                # the client is not sensible
+                # kick it
+                logger.error('Insensible client (too many pending keepalive responses). Kick it.')
                 break
-            elif command == b'MSG':
-                logger.info('Receiving message...')
-                message_length = int.from_bytes(read_exactly(sock, 8), NETWORK_BYTEORDER, signed=False)
-                logger.info('Length: %d', message_length)
-                message = read_exactly(sock, message_length)
-                logger.info('Topic: %s, Message: %s', topic_id, message)
-                dispatcher.publish(message, topic_id)
-            else:
-                raise InvalidMessageError(f'Invalid command from client: {command}')
-    except IncompleteReadError:
-        logger.info('Connection is closed.')
+            logger.info('Send NOP. (keepalive)')
+            sock.sendall(b'NOP')
+            logger.info('NOP is sent.')
+            pending_keepalive_count += 1
+            continue
+        command = read_exactly(sock, 3)
+        if command == b'NOP':
+            logger.info('Client NOP.')
+            sock.sendall(b'NIL')
+            logger.info('Responded with NIL.')
+        elif command == b'NIL':
+            logger.info('Client NIL. Client is OK.')
+            pending_keepalive_count = 0
+            pass
+        elif command == b'BYE':
+            logger.info('Client BYE. Disconnecting.')
+            break
+        elif command == b'MSG':
+            logger.info('Receiving new message from client...')
+            message_length = int.from_bytes(read_exactly(sock, 8), NETWORK_BYTEORDER, signed=False)
+            logger.info('Message length: %d byte(s).', message_length)
+            message = read_exactly(sock, message_length)
+            logger.info('Topic: %s, Message: %s.', topic_id, message)
+            dispatcher.publish(message, topic_id)
+        else:
+            raise InvalidMessageError(f'Invalid command from client: {command}')
 
 
 def _subscribe(sock: socket.socket, addr, dispatcher: MessageDispatcher, subscriber_id: int, pattern: str,
-               keep_alive: float, logger, max_pending_keepalive: int = 3):
+               keep_alive: float, max_pending_keepalive: int = 3):
+    logger = logging.getLogger('subscribe,%s:%d' % addr)
     if keep_alive > 0:
         # sock.settimeout(keep_alive)
         logger.info('Keepalive is enabled. Set socket timeout to %fs.', keep_alive)
@@ -120,7 +119,7 @@ def _subscribe(sock: socket.socket, addr, dispatcher: MessageDispatcher, subscri
                         sock.sendall(len(message).to_bytes(8, NETWORK_BYTEORDER, signed=False))
                         sock.sendall(message)
     except Exception:
-        logger.exception('Unexpected exception.')
+        logger.exception('An exception occurred. Disconnecting.')
     finally:
         dispatcher.unsubscribe(subscriber_id)
         rsock.close()
@@ -128,9 +127,9 @@ def _subscribe(sock: socket.socket, addr, dispatcher: MessageDispatcher, subscri
 
 
 def handle_client(sock: socket.socket, addr, dispatcher: MessageDispatcher, keep_alive: float):
-    logger = logging.getLogger('%s:%d' % addr)
+    logger = logging.getLogger('handle_client,%s:%d' % addr)
     try:
-        logger.info('Handling client...')
+        logger.info('Accept inbound connection from %s:%d.' % addr)
 
         if read_exactly(sock, 4) != b'PSMB':
             logger.info('Bad protocol magic.')
@@ -149,7 +148,7 @@ def handle_client(sock: socket.socket, addr, dispatcher: MessageDispatcher, keep
             return
 
         sock.sendall(b'OK\0\x00\x00\x00\x00')
-        logger.info('Handshake complete.')
+        logger.info('Complete handshaking.')
 
         while True:
             mode = read_exactly(sock, 3)
@@ -161,8 +160,8 @@ def handle_client(sock: socket.socket, addr, dispatcher: MessageDispatcher, keep
                     sock.sendall(b'FAILED\0' + b'Cannot decode topic id string with ASCII.\0')
                     continue
                 sock.sendall(b'OK\0')
-                logger.info(f'Switched to PUBLISH mode. Topic is {topic_id}.')
-                _publish(sock, addr, dispatcher, topic_id, keep_alive, logger, protocol=protocol)
+                logger.info(f'Switch to PUBLISH mode. Topic is {topic_id}.')
+                _publish(sock, addr, dispatcher, topic_id, keep_alive, protocol=protocol)
                 break
             elif mode == b'SUB':
                 options = int.from_bytes(read_exactly(sock, 4), NETWORK_BYTEORDER, signed=False)
@@ -178,8 +177,8 @@ def handle_client(sock: socket.socket, addr, dispatcher: MessageDispatcher, keep
                     sock.sendall(b'FAILED\0' + b'Invalid pattern string.\0')
                     continue
                 sock.sendall(b'OK\0')
-                logger.info(f'Switched to SUBSCRIBE mode. Subscriber is {subscriber_id}.')
-                _subscribe(sock, addr, dispatcher, subscriber_id, id_pattern, keep_alive, logger)
+                logger.info(f'Switch to SUBSCRIBE mode. Subscriber is {subscriber_id}.')
+                _subscribe(sock, addr, dispatcher, subscriber_id, id_pattern, keep_alive)
                 break
             else:
                 sock.sendall(b'BAD COMMAND\0')
@@ -192,4 +191,4 @@ def handle_client(sock: socket.socket, addr, dispatcher: MessageDispatcher, keep
         logger.exception('Unexpected exception.')
     finally:
         sock.close()
-        logger.info('Handler exited.')
+        logger.info('Connection is closed.')
