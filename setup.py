0dc47aec465d3963745ecaa1ffdcdc5ce509db5c
Keuin <keuinx@gmail.com>
2022-03-18 11:21:32 +0800

    Bump version.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ github_address = 'https://github.com/hit-mc/pypsmb'
 
 setup(
     name='pypsmb',
-    version='0.0.7',
+    version='0.0.8',
     description='psmb server implemented in Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
